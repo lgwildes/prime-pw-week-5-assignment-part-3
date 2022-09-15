ee2de6903e2c5440cdf5b89babc5293e098382ea
Lydia Wildes <lydiawildes1@gmail.com>
2022-09-15 13:27:05 -0500

    reformatted console.logs and got help on accessing object items in array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,28 +1,40 @@
 console.log('***** Music Collection *****')
 let collection = [];
+
 function addToCollection (title, artist, yearPublished) {
     let album = {
         title: title ,
         artist: artist ,
         yearPublished: yearPublished
-   
-}
+    }
+      
 collection.push(album) ;
-console.log(album);
-}
-addToCollection('Mmmhmm','Relient k', 2004);
-console.log('Added Mmmhmm' );
-addToCollection('Five Score and Seven Years Ago', 'Relient k', 2007);
-console.log('Added Five Score and Seven Years Ago')
-addToCollection('The Beautiful Letdown', 'Switchfoot', 2003);
-console.log('Added The Beautiful Letdown')
-addToCollection('Nothing is Sound', 'Switchfoot', 2005);
-console.log('Added Nothing is Sound')
-addToCollection('Oh Gravity!', 'Switchfoot', 2006);
-console.log('Added Oh Gravity!')
-addToCollection('Viva La Vida', 'Coldplay', 2008);
-console.log('Added Viva La Vida')
-addToCollection('X & Y', 'Coldplay', 2005)
-console.log('Added X & Y')
-console.log(collection)
+return album ;
 
+
+}
+
+console.log('Added Mmmhmm' , addToCollection('Mmmhmm','Relient k', 2004));
+
+console.log('Added Five Score and Seven Years Ago' ,addToCollection('Five Score and Seven Years Ago', 'Relient k', 2007))
+
+console.log('Added The Beautiful Letdown' ,addToCollection('The Beautiful Letdown', 'Switchfoot', 2003))
+
+console.log('Added Nothing is Sound' ,addToCollection('Nothing is Sound', 'Switchfoot', 2005))
+
+console.log('Added Oh Gravity!' ,addToCollection('Oh Gravity!', 'Switchfoot', 2006))
+
+console.log('Added Viva La Vida' ,addToCollection('Viva La Vida', 'Coldplay', 2008));
+;
+console.log('Added X & Y' ,addToCollection('X & Y', 'Coldplay', 2005))
+console.log(collection);
+
+function showCollection(array){
+        console.log('the number of items in the array are: ', array.length);
+        for(i=0; i<array.length; i++) {
+            console.log(array[i].title)
+          
+           
+}
+}
+showCollection(collection)
