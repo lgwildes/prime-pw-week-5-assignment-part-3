5f1834d775b9bbc2dac5160164668c796a814690
Lydia Wildes <lydiawildes1@gmail.com>
2022-09-15 09:35:07 -0500

    attempted first step, need to troubleshoot

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+let collection = [];
+function addToCollection (title, artist, yearPublished) {
+    let album = {
+        title: '' ,
+        artist: '' ,
+        yearPublished: ''
+   
+}
+collection.push(album)
+console.log(album)
+}
+
+console.log(addToCollection('Mmhmmm', 'Relient k', 2004))
