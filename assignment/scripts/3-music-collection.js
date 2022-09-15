cf1dcb94661102c21e9b8c9b473d6ab50eb3264d
Lydia Wildes <lydiawildes1@gmail.com>
2022-09-15 11:28:41 -0500

    added console logs and each album.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -7,8 +7,22 @@ function addToCollection (title, artist, yearPublished) {
         yearPublished: yearPublished
    
 }
-collection.push(album)
-return album
+collection.push(album) ;
+console.log(album);
 }
-addToCollection('Mmmhmm','Relient k', 2004)
+addToCollection('Mmmhmm','Relient k', 2004);
+console.log('Added Mmmhmm' );
+addToCollection('Five Score and Seven Years Ago', 'Relient k', 2007);
+console.log('Added Five Score and Seven Years Ago')
+addToCollection('The Beautiful Letdown', 'Switchfoot', 2003);
+console.log('Added The Beautiful Letdown')
+addToCollection('Nothing is Sound', 'Switchfoot', 2005);
+console.log('Added Nothing is Sound')
+addToCollection('Oh Gravity!', 'Switchfoot', 2006);
+console.log('Added Oh Gravity!')
+addToCollection('Viva La Vida', 'Coldplay', 2008);
+console.log('Added Viva La Vida')
+addToCollection('X & Y', 'Coldplay', 2005)
+console.log('Added X & Y')
 console.log(collection)
+
--- readme.md
+++ readme.md
@@ -25,7 +25,7 @@ Update the `3-music-collection.js` file to do the following:
 
 - Test the `addToCollection` function:
   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
-  - Console.log each album as added using the returned value.
+  - Console.log each album as added using the returned value. 
   - After all are added, console.log the `collection` array.
 
 - Add a function named `showCollection`. This function should:
