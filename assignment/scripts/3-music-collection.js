5c375eebea71d3b0ad1c7ea171013997e40ce6a0
Lydia Wildes <lydiawildes1@gmail.com>
2022-09-17 13:27:01 -0500

    added tracks to albums, reformatted for console aesthetic. Need to figure out search for ALL iterations

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,7 @@
 console.log('***** Music Collection *****')
 let collection = [];
 
+//creating function to add albums and their info to collection array
 function addToCollection (newTitle, newArtist, newYearPublished, newTracks) {
     let album = {
         title: newTitle ,
@@ -10,84 +11,103 @@ function addToCollection (newTitle, newArtist, newYearPublished, newTracks) {
     }
     for(i=0; i<newTracks.length; i++) {
         album.tracks.push(newTracks[i])
- //       console.log('song and duration' ,newTracks[i].song, newTracks[i].duration)
     }  
 collection.push(album) ;
 return album ;
 
 }
+//Mmhmm logging all tracks ---CUZ COMPLETIONISM
+console.log('Added Mmhmm' , addToCollection('Mmhmm','Relient k', '2004', [{trackName:`The One I'm Waiting For`, duration: '3:02'},{trackName:`Be My Escape`, duration: '4:00'},{trackName:`High of 75`, duration: '2:27'},{trackName:`I So Hate Consequences`, duration: '4:01'},{trackName:`The Only Thing Worse Than Beating a Dead Horse Is Betting On One`, duration: '1:13'},{trackName:`My Girl's Ex-Boyfriend`, duration: '2:28'},{trackName:`More Than Useless`, duration: '3:50'},{trackName:`Which to Bury; Us or the Hatchet?`, duration: '4:11'},{trackName:`Let It All Out`, duration: '4:21'},{trackName:`Who I Am Hates Who I've Been`, duration: '3:52'},{trackName:`Maintain Consciousness`, duration: '2:52'},{trackName:`This Week the Trend`, duration: '2:59'},{trackName:`Life After Death and Taxes`, duration: '4:23'},{trackName:`When I Go Down`, duration: '6:42'}]));
 
-console.log('Added Mmmhmm' , addToCollection('Mmmhmm','Relient k', 2004, [{song:`The One I'm Waiting For`, duration: '3:02'},{song:`Be My Escape`, duration: '4:00'}]));
+//Five Score and Seven Years Ago ---STILL GOING HARD
+console.log('Added Five Score and Seven Years Ago' ,addToCollection('Five Score and Seven Years Ago', 'Relient k', '2007',[{trackName:`Pleading the Fifth`, duration: '1:13'},{trackName:`Come Right out and Say It`, duration: '3:00'},{trackName:`I Need You`, duration: '3:18'},{trackName:`The Best Thing`, duration: '3:28'},{trackName:`Forgiven`, duration: '4:05'},{trackName:`Must Have Done Something Right`, duration: '3:19'},{trackName:`Give Until There's Nothing Left`, duration: '3:27'},{trackName:`Devastation and Reform`, duration: '3:41'},{trackName:`I'm Taking You with Me`, duration: '3:28'},{trackName:`Faking My Own Suicide`, duration: '3:23'},{trackName:`Crayons Can Melt on Us for All I Care`, duration: '0:12'},{trackName:`Bite My Tongue`, duration: '3:30'},{trackName:`Up and Up`, duration: '4:03'},{trackName:`Deathbed`, duration: '11:05'}]));
 
-console.log('Added Five Score and Seven Years Ago' ,addToCollection('Five Score and Seven Years Ago', 'Relient k', 2007,[{song:`The One I'm Waiting For`, duration: '3:02'},{song:`Be My Escape`, duration: '4:00'}]));
+//The Beautiful Letdown ---true to name I'm sticking to two tracks from now on...sorry for the letdown
+console.log('Added The Beautiful Letdown' ,addToCollection('The Beautiful Letdown', 'Switchfoot', '2003',[{trackName:`Meant to Live`, duration: '3:21'},{trackName:`This is Your Life`, duration: '4:19'}]));
 
-console.log('Added The Beautiful Letdown' ,addToCollection('The Beautiful Letdown', 'Switchfoot', 2003,[{song:`The One I'm Waiting For`, duration: '3:02'},{song:`Be My Escape`, duration: '4:00'}]));
+//Nothing is Sound
+console.log('Added Nothing is Sound' ,addToCollection('Nothing is Sound', 'Switchfoot', '2005',[{trackName:`Lonely Nation`, duration: '3:45'},{trackName:`Stars`, duration: '4:20'}]));
 
-console.log('Added Nothing is Sound' ,addToCollection('Nothing is Sound', 'Switchfoot', 2005,[{song:`Lonely Nation`, duration: '3:02'},{song:`Be My Escape`, duration: '4:00'}]));
+//Oh! Gravity. 
+console.log('Added Oh! Gravity.' ,addToCollection('Oh! Gravity.', 'Switchfoot', '2006',[{trackName:`Oh! Gravity.`, duration: '2:30'},{trackName:`American Dream`, duration: '3:09'}]));
 
-console.log('Added Oh Gravity!' ,addToCollection('Oh Gravity!', 'Switchfoot', 2006,[{song:`The One I'm Waiting For`, duration: '3:02'},{song:`Be My Escape`, duration: '4:00'}]));
+//Viva La Vida
+console.log('Added Viva La Vida' ,addToCollection('Viva La Vida', 'Coldplay', '2008',[{trackName:`Life in Technicolor`, duration: '2:29'},{trackName:`Cemeteries of London`, duration: '3:21'}]));
 
-console.log('Added Viva La Vida' ,addToCollection('Viva La Vida', 'Coldplay', 2008,[{song:`The One I'm Waiting For`, duration: '3:02'},{song:`Be My Escape`, duration: '4:00'}]));
-;
-console.log('Added X & Y' ,addToCollection('X & Y', 'Coldplay', 2005,[{song:`The One I'm Waiting For`, duration: '3:02'},{song:`Be My Escape`, duration: '4:00'}]));
-console.log(collection);
+//X&Y
+console.log('Added X&Y' ,addToCollection('X&Y', 'Coldplay', '2005',[{trackName:`Square One`, duration: '4:47'},{trackName:`What If`, duration: '4:58'}]));
 
+//console.log collection array
+console.log('===== Logging collection array ====',collection);
 
+//creating function to take array parameter
 function showCollection(array){
-    console.log('the number of items in the array are: ', array.length);
+    console.log('the number of items in the array are: ', array.length); //console.log number of items in array
     for(let i=0; i<array.length; i++) {
-        console.log(array[i].title, 'by', array[i].artist,', published in', array[i].yearPublished);  
+        console.log(array[i].title, 'by', array[i].artist,', published in', array[i].yearPublished); //Looping over array and console.logging each album's information 'TITLE by ARTIST, published in YEAR'
         for(let n=0; n<array[i].tracks.length; n++) {
         
-            console.log(' ' + (n+1) +'. '+ array[i].tracks[n].song + ': ' + array[i].tracks[n].duration)
-        }            
+            console.log(' ' + (n+1) +'. '+ array[i].tracks[n].trackName + ': ' + array[i].tracks[n].duration)
+        } //console.logging tracks and indented for readability. TRACK NUMBER. NAME: DDURATION       
     }
 
 }
 
 console.log('==== running showCollection ====');
-showCollection(collection)
+showCollection(collection) //collection formatted as string for readability by user not as just an array
 
-
+//creating function to search collection for albums by specific artist
 function findByArtist(artist, array){
-    let foundArtist = [];
+    let foundByArtist = []; //this shows which albums by this artist are in the collection
     for (let i=0; i<array.length; i++){
         if (artist === array[i].artist){
-            foundArtist.push(array[i])
+            foundByArtist.push(array[i]) //add album to foundByArtist array
         }
         
     }
-    return foundArtist;
+    return foundByArtist; //returns array of any/all albums by artist in collection
 }
 
-console.log('Searching for Switchfoot: ' ,findByArtist('Switchfoot', collection));
-console.log('Searching for ACDC: ' ,findByArtist('ACDC', collection));
+console.log('Searching for Switchfoot: ' ,findByArtist('Switchfoot', collection)); //expect array of 3 albums
+console.log('Searching for ACDC: ' ,findByArtist('ACDC', collection)); //expect empty array
 
 //STRETCH TIME BABY
 
-
+//creating search function which has a search object (which has properties for artist, year and trackName) and the collection array as parameters
+let searchMatch = [];
 function search( searchingObject, collection){
     if ( !searchingObject){
-        console.log('no search object found, but check out this awesome collection');
+        console.log('    Sadly, there was no search object found, but check out this awesome collection!');
         return collection;
-        //This checks to see if there is no match to any object searched and returns the collection plus a consiliatory message. 
+        //This checks to see if there is no object searched and returns the collection plus a consiliatory message. 
     }
+
     let searchArtist = searchingObject.artist;
     let searchYear = searchingObject.year;
-    let searchSong = searchingObject.song;
+    let searchtrackName = searchingObject.trackName;
+     //created variables of the serach object properties to make it easier for me to read and conceptualize within my for loop.
    
-    let searchMatch = [];
-    for (let i=0; i<collection.length; i++){
-        if(!search.song){
+    
+    //looping through the collection
+    for (let i=0; i<collection.length; i++)  { 
+        if(!search.trackName){
             if (searchArtist === collection[i].artist && searchYear === collection[i].yearPublished ){
             searchMatch.push(collection[i])
-            
+            //pushing all items (albums) that match the criteria (artist and year) searched
              }
         }
+        
+        else if(searchArtist != collection[i].artist || searchYear != collection[i].yearPublished) {
+            return searchMatch
+        }
+        
+        
         else {
-            for(n=0; n<collection[i].tracks.length; n++){
-                 if (searchArtist === collection[i].artist && searchYear === collection[i].yearPublished && searchSong ===collection[i].tracks[n].song){
+            //looping through the tracks to search for an album containing a specific trackName
+            for(n=0; n<collection[i].tracks.length; n++) {
+                 if (searchArtist === collection[i].artist && searchYear === collection[i].yearPublished && searchtrackName ===collection[i].tracks[n].trackName){
                     searchMatch.push(collection[i]);
+                    //pushing album that contains the trackName searched
                 } 
             }
         }  
@@ -95,25 +115,33 @@ function search( searchingObject, collection){
         
     return searchMatch;
 }
-let foundItems = [];
+let foundItems = []; //This array displays the object 'title' within the array item which matches the search, this way instead of listing the entire array, it displays just the album.
 
-console.log('Searching for Switchfoot 2005' );
-foundItems = search({artist: 'Switchfoot', year: 2005, song:'' }, collection);
+console.log('==== Searching for Switchfoot 2005 ====' ); //searching for album artist and year
+foundItems = search({artist: 'Switchfoot', year: '2005', trackName:''}, collection);
 for(i=0; i<foundItems.length; i++){
-    console.log( '   ', foundItems[i].title )
+    console.log( '   ', foundItems[i].title ) //logs album by artist in this year
 }
 
-console.log('Searching for song: Lonely Nation') 
-foundItems = search({artist: 'Switchfoot', year: 2005, song: 'Lonely Nation'}, collection);
+console.log('==== Searching for album which contains the track Be My Escape ====') //searching for album which contains this track.
+foundItems = search({artist: 'Relient k', year: '2004', trackName: 'Be My Escape'}, collection);
 for(i=0; i<foundItems.length; i++){
-    console.log( '   ', foundItems[i].title )
+    console.log( '   ', foundItems[i].title ) //logs album containing track
 }
 
-console.log('Searching for empty, expect collection list')
+console.log('==== Searching with no object, expect collection list ====')
 foundItems = search(null, collection);
 for(i=0; i<foundItems.length; i++){
-    console.log( '   ', foundItems[i].title )
+    console.log( '   ', foundItems[i].title ) //logs consiliatory message and collection list
 }
+
+console.log('==== Searching for album with wrong year, expect empty array ====')
+foundItems = search({artist: 'ACDC', year: '2004', trackName: 'Be My Escape'}, collection);
+
+for(i=0; i<foundItems.length; i++){
+    console.log( '   ', foundItems[i].title ) 
+}
+//logs consiliatory message and collection list
 
 
 //console.log('Searching for Switchfoot 2005' , search({artist: 'Switchfoot', year: 2005}, collection));
